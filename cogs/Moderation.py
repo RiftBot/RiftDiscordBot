717d37c77f8a7e13b67acef75c7e00a44f130ed3
Robin Long <99682144+RiftBot@users.noreply.github.com>
2022-02-14 21:22:55 +0000

    Update moderation.py, added the command ban and added some changes to kick

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -10,7 +10,7 @@ class Moderation(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member : discord.Member=None, *, reason=None):
         if reason == None and member == None:
-            embed = discord.Embed(title="Error!", description="This command has not been ran properly. e.g !kick @member reason", color=discord.Color.red())
+            embed = discord.Embed(title="Error!", description="This command has not been ran properly.\ne.g !kick @member reason", color=discord.Color.red())
             await ctx.send(embed = embed)
         else:
             embed = discord.Embed(title="Success!", description="The member has been kicked!", color=discord.Color.green())
@@ -29,6 +29,30 @@ class Moderation(commands.Cog):
                 embed = discord.Embed(title="Error!", description="Sorry but the embed could not be sent to the member!", color=discord.Color.red())
                 await ctx.send(embed=embed)
                 await member.kick(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member : discord.Member=None, *, reason=None):
+        if reason == None and member == None:
+            embed = discord.Embed(title="Error!", description="This command has not been ran properly.\ne.g !ban @member reason", color=discord.Color.red())
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="Success!", description="The member has been banned!", color=discord.Color.green())
+            embed.add_field(name="Mmeber", value=f"{member.mention}", inline=False)
+            embed.add_field(name="Reason", value=f"{reason}", inline=False)
+            embed.set_footer(text=f"{today}")
+            await ctx.send(embed=embed)
+            memberEmbed = discord.Embed(title="Kicked!", description=f"You have been banned from guild {ctx.guild.name}", color=discord.Color.red())
+            memberEmbed.add_field(name="Staff Member", value=f"{ctx.author.mention}", inline=False)
+            memberEmbed.add_field(name="Reason", value=f"{reason}", inline=False)
+            memberEmbed.set_footer(text=f"{today}", icon_url=f"{ctx.guild.icon_url}")
+            try:
+                await member.send(embed=memberEmbed)
+                await member.ban(reason=reason)
+            except Exception:
+                embed = discord.Embed(title="Error!", description="Sorry but the embed could not be sent to the member!", color=discord.Color.red())
+                await ctx.send(embed=embed)
+                await member.ban(reason=reason)
 
 
 def setup(bot):
