9685611053047f3f1626c1f353441c3ed960d459
Robin Long <99682144+RiftBot@users.noreply.github.com>
2022-02-14 19:36:49 +0000

    Added functionality for cogs.
    Added functionality for cogs.
    Meaning commands can be sorted into separate files and groups.

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
 import discord
 from discord.ext import commands
 import config
+import os
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
     print("Bot is logged in!")
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f"Loaded cog\n{filename[:-3]}\n")
+    else:
+        print(f"This file in the cogs folder does not end with .py!\n{filename[:-3]}\n")
 
 bot.run(config.token)
